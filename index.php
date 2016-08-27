a158c3e799013d73524dd96f71c351e42b6a8b44
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-08-27 16:31:51 -0400

    Populate index.php file w/basic HTML start code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+      <meta charset="utf-8">
+        <title>Christina Hendricks Blank Template</title>
+    </head>
+
+    <body>
+      
+      <h1>Blank WordPress Template</h1>
+
+      <p>This is my WordPress template.</p>
+
+    </body>
+
+  </html>
