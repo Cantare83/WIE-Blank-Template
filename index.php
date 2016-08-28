cf819c1925c744adf985abb0da7c9e344cf3be90
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-08-28 18:39:51 -0400

    Add links in index and styles for all stylesheets

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
     <head>
       <meta charset="utf-8">
-        <title>Christina Hendricks Blank Template</title>
+      <title>Christina Hendricks Blank Template</title>
+        <!-- Links to our Style.css file -->
+      <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
     </head>
-
     <body>
-      
+
       <h1>Blank WordPress Template</h1>
 
       <p>This is my WordPress template.</p>
