a06c8a1288afa537f42c947c51078399460173ef
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-08-28 18:53:45 -0400

    Add basic HTML5 syntax

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-      <meta charset="utf-8">
-      <title>Christina Hendricks Blank Template</title>
-        <!-- Links to our Style.css file -->
-      <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+        <meta charset="utf-8">
+        <title>Christina Hendricks Blank Template</title>
+          <!-- Links to our Style.css file -->
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
     </head>
     <body>
-
-      <h1>Blank WordPress Template</h1>
-
-      <p>This is my WordPress template.</p>
-
+      <div class="container">
+          <header>
+              <h1>Christina Hendricks Blank WordPress Template</h1>
+              <p>This is my WordPress template.</p>
+          </header>
+          <section>
+              <h2>Section Content</h2>
+              <p>
+                This is some cool section content
+              </p>
+          </section>
+          <footer>
+              <h2>This is the Footer</h2>
+              <p>
+                This is some cool footer content
+              </p>
+          </footer>
+      </div>
     </body>
-
   </html>
