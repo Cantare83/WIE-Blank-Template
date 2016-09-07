a5f599bd35820b835f47f5312bf19038b4689a77
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-09-07 18:08:08 -0400

    separate out footer and header files and link to index with php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Christina Hendricks Blank Template</title>
+          <!-- Links to our Style.css file -->
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    </head>
+    <body>
+      <div class="container"> <!-- Begin Container Div -->
+        <header class="row">
+            <div class="twelve columns">
+                <h1>Christina Hendricks Blank WordPress Template</h1>
+                <p>This is my WordPress template.</p>
+            </div>
+        </header>
