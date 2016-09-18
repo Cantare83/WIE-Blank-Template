09e0761a96cf2486fd6d89346587aa29ac3518d1
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-09-18 16:50:49 -0400

    Add featured images and thumbnails to theme

--- single.php
+++ single.php
@@ -6,7 +6,13 @@
 <!-- Begin Page PHP -->
             <?php if( have_posts() ) {
               while( have_posts() ) {
-                the_post(); ?>
+                the_post();
+
+                if ( has_post_thumbnail() ) { ?>
+                    <div class="post-thumbnail">
+                      <?php the_post_thumbnail('large'); ?>
+                    </div>
+                <?php } ?>
 
                 <h3><?php the_title(); ?></h3>
                 <?php the_content(); ?>
